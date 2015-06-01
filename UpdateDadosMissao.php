27b33797ff2d46186a145813fb0622658a27003a
Silvana Nakle <silvana.nakle@gmail.com>
2015-06-01 15:11:06 -0300

    teste de comitt do netbeans

---
+++ UpdateDadosMissao.php
@@ -0,0 +1,24 @@
+<?php
+require "header.php";
+session_start();
+if(isset($_SESSION['username']))
+	$username = $_SESSION['username'];
+if(isset($_SESSION['password']))
+	$password = $_SESSION['password'];
+if(!(empty($username) OR empty($password)))
+{ 
+include "CrudMissoes.php";
+
+	$idMissao = $_REQUEST['idMissao'];
+	$idProjeto = $_REQUEST['idFase'];
+	$nome = $_POST["nome"];
+	$descricao = $_POST["descricao"];
+
+	updateMissao($idFase, $nome, $descricao, $idProjeto);
+
+}else{
+	echo "usuario não cadastrado, faça seu cadastro";
+	echo "<a href= '../index.html'> Voltar </a>";
+	}
+?>
+
